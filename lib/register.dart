29170cf0fe7a7124530697c69c4eb76566fc1d45
V1shall <160301234+V1shall@users.noreply.github.com>
2024-03-16 15:26:30 +0530

    hello

--- lib/register.dart
+++ lib/register.dart
@@ -2,7 +2,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:loginpage/loginscreen.dart';
-import 'package:loginpage/qrcode.dart';
 import 'package:http/http.dart' as http;
 
 class Register extends StatefulWidget {
